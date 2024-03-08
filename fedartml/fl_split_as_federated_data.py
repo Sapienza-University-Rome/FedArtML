0cd75c4bcf5a1e09b2ad2014d0cd66f48c3d2913
damjimenezgu <damjimenezgu@unal.edu.co>
2024-03-08 17:37:41 +0100

    change main to master

--- fedartml/fl_split_as_federated_data.py
+++ fedartml/fl_split_as_federated_data.py
@@ -545,11 +545,33 @@ class SplitAsFederatedData:
         spatemp_fed_data : dict
             Contains categories of the spatio-temporal variable for each local node (client) after federating the data. It is generated only when spa_temp_skew_method = "st-dirichlet".
 
-        Note: When creating federated data and setting heterogeneous distributions (i.e. high values of percent_noniid or small values of alpha), it is more likely the clients hold examples from only one class.
-        Then, two cases (for labels and features) are returned as output for fed_data and distances:
+        Note: When creating federated data and setting heterogeneous distributions (i.e. high values of percent_noniid or small values of alpha), it is more likely the clients hold examples from only one class. Then, two cases (for labels and features) are returned as output for fed_data and distances:
             - "with_class_completion": In this case, the clients are completed with one (random) example of each missing class for each client to have all the label's classes.
             - "without_class_completion": In this case, the clients are NOT completed with one (random) example of each missing class. Consequently, summing the number of examples of each client results in the same number of total examples (number of rows in image_list).
 
+        References
+        ----------
+            .. [1] (dirichlet) Tao Lin∗, Lingjing Kong∗, Sebastian U. Stich, Martin Jaggi. (2020). Ensemble Distillation for Robust Model Fusion in Federated Learning0
+               https://proceedings.neurips.cc/paper/2020/file/18df51b97ccd68128e994804f3eccc87-Supplemental.pdf
+            .. [2] (percent_noniid) Hsieh, K., Phanishayee, A., Mutlu, O., & Gibbons, P. (2020, November). The non-iid data quagmire of decentralized machine learning. In International Conference on Machine Learning (pp. 4387-4398).PMLR.
+               https://proceedings.mlr.press/v119/hsieh20a/hsieh20a.pdf
+            .. [3] (gaussian noise) Li, Q., Diao, Y., Chen, Q., & He, B. (2022, May). Federated learning on non-iid data silos: An experimental study. In 2022 IEEE 38th International Conference on Data Engineering (ICDE) (pp. 965-978). IEEE.
+        Examples
+        --------
+        >>> from fedartml import SplitAsFederatedData
+        >>> from keras.datasets import mnist
+        >>> (train_X, train_y), (test_X, test_y) = mnist.load_data()
+        >>> my_federater = SplitAsFederatedData(random_state=0)
+        >>>
+        >>> # Using percent_noniid method
+        >>> clients_glob, list_ids_sampled, miss_class_per_node, distances =
+        >>>     my_federater.create_clients(image_list=train_X, label_list=train_y, num_clients=4,
+        >>>     prefix_cli='Local_node',method="percent_noniid", percent_noniid=0)
+        >>>
+        >>> # Using dirichlet method
+        >>> clients_glob, list_ids_sampled, miss_class_per_node, distances =
+        >>>     my_federater.create_clients(image_list=train_X, label_list=train_y, num_clients=4,
+        >>>     prefix_cli='Local_node',method="dirichlet", alpha=1000)
         """
         # create a list of client names
         client_names = ['{}_{}'.format(prefix_cli, i + 1) for i in range(num_clients)]
